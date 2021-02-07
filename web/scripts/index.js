63047bc93898c5deb92bb736ffd79d493691c7ba
thiryr <renaud.thiry@student.unamur.be>
2021-02-07 20:51:19 +0100

    more OOP, draw pawns

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -1,28 +1,23 @@
-//"constants" for drawing
-let Canvas = null
-let Context = null
-
 //CONSTANTS
+//C_board
+let BOARD = null
+
 //C_Color
 const Red = "rgba(255, 0, 0, 1)"
 const Yellow = "rgba(255, 155, 0, 1)"
 const Blue = "rgba(0, 0, 255, 1)"
 const Green = "rgba(0, 255, 0, 1)"
 const Grey = "rgba(100,100,100,1)"
+const Black = "rgba(0,0,0,1)"
 
 const SpaceColor = Grey
+const WallColor = Black
+
+//C_Display
+const PawnRatio = 0.5
 
 //C_Fonts
 const Font = '20px serif'
-
-//C_Display
-const SpaceSize = 30;
-const SlitSize = 5;
-const BoardOffsetX = 50;
-const BoardOffsetY = 50;
-
-//C_Board
-const BoardSize = 9;
 
 //C_Players
 const BaseWalls = 20;
@@ -45,50 +40,167 @@ function absolute(x) {
 
 //startup
 $(window).on("load", () => {
-        Canvas = $("#gameCanvas")[0]
-        Context = Canvas.getContext("2d")
-        initBoard(BoardSize)
-        initPawns(BoardSize)
-        drawBoard(Context)
+        let canvas = $("#gameCanvas")[0]
+        let game_board = new Board(canvas, 50, 50)
+        game_board.init_board()
+        game_board.draw()
+        BOARD = game_board
     }
 
 )
 
-//useful global variables
-let Board = []
-let Pawns = []
-let Walls = []
 
-function initBoard(size) {
-    for (let x = 0; x < size; x++) {
-        Board.push([])
-        for (let y = 0; y < size; y++) {
-            Board[x].push(new PawnSpace(x, y))
-        }
+
+
+
+class Board {
+    constructor(canv, x_offset, y_offset) {
+        this.canvas = canv
+        this.context = canv.getContext("2d")
+
+        this.space_size = 30;
+        this.slit_size = 5;
+        this.x_offset = x_offset;
+        this.y_offset = y_offset;
+
+        this.size = 9;
+
+        //board_rep is a 2D-array of pawnSpaces
+        this.board_rep = this.init_board()
+        this.pawns = this.init_pawns()
+        this.walls = []
+
     }
-}
 
-function initPawns(size) {
+    init_board() {
+        let board_rep = []
+        let size = this.size
 
-    let s_half = intDiv(size, 2)
-        //space and goals are set after this
+        for (let x = 0; x < size; x++) {
+            board_rep.push([])
+            for (let y = 0; y < size; y++) {
+                board_rep[x].push(new PawnSpace(this, x, y))
+            }
+        }
 
-    let red = new Pawn(null, Red, null, null, false, BaseWalls);
-    let blue = new Pawn(null, Blue, null, null, true, BaseWalls);
-    let green = new Pawn(null, Green, null, null, false, BaseWalls);
-    let yellow = new Pawn(null, Yellow, null, null, true, BaseWalls);
+        return board_rep
+    }
 
-    //add to Pawns and set parameters in the same order
-    Pawns = [red, blue, green, yellow]
-    let spaces = [Board[s_half][0], Board[0][s_half], Board[s_half][size - 1], Board[size - 1][s_half]];
-    let goals_y = [size - 1, 0, size - 1, 0]
-    let goals_x = [0, size - 1, 0, size - 1]
+    init_pawns() {
+        let board = this.board_rep
 
-    for (let i = 0; i < Pawns.length; i++) {
-        let pawn = Pawns[i]
-        pawn.setSpace(spaces[i])
-        pawn.goal_x = goals_x[i]
-        pawn.goal_y = goals_y[i]
+        let size = this.size
+        let pawns = []
+
+        let s_half = intDiv(size, 2)
+            //space and goals are set after this
+
+        let red = new Pawn(null, Red, null, null, false, BaseWalls);
+        let blue = new Pawn(null, Blue, null, null, true, BaseWalls);
+        let green = new Pawn(null, Green, null, null, false, BaseWalls);
+        let yellow = new Pawn(null, Yellow, null, null, true, BaseWalls);
+
+        //add to Pawns and set parameters in the same order
+        pawns = [red, blue, green, yellow]
+        let spaces = [board[s_half][0], board[0][s_half], board[s_half][size - 1], board[size - 1][s_half]];
+        let goals_y = [size - 1, 0, size - 1, 0]
+        let goals_x = [0, size - 1, 0, size - 1]
+
+        for (let i = 0; i < pawns.length; i++) {
+            let pawn = pawns[i]
+            pawn.setSpace(spaces[i])
+            pawn.goal_x = goals_x[i]
+            pawn.goal_y = goals_y[i]
+        }
+        return pawns
+    }
+
+    draw() {
+        let slit_size = this.slit_size
+        let space_size = this.space_size
+        let size = this.size
+
+        let ctx = this.context
+
+        let x_offset = this.x_offset
+        let y_offset = this.y_offset
+
+        this.drawPawnSpaces()
+
+        let H_x = x_offset + intDiv(space_size, 2) - slit_size
+        let H_y = y_offset + intDiv(space_size, 2) + ((1 + size) * slit_size) + (size * space_size)
+        let V_x = x_offset - space_size
+        let V_y = y_offset + ((1 + size) * slit_size) + (size * space_size) - intDiv(space_size, 2)
+        this.drawAxis(V_x, V_y, 1, 0, -1, false)
+        this.drawAxis(H_x, H_y, 'A', 1, 0, true)
+
+
+        let pawns = this.pawns
+        for (let i = 0; i < pawns.length; i++) {
+            pawns[i].draw()
+        }
+
+    }
+
+    /**
+     * draws axis with sequential characters/integer for each grid row/collumn
+     * @param {int/char} init_char, initial character/number (if character, use alphabet) 
+     * @param {int} x_inc, incrementation factor for x coordinate in number of grid spaces
+     * @param {int} y_inc, ^same for y
+     * @param {bool} alphabet, false if displaying sequential integers 
+     * @example drawAxis(ctx, 0, 0, 12, -1, 0, false) =>   12 13 14
+     */
+    drawAxis(start_x, start_y, init_char, x_inc, y_inc, alphabet) {
+
+        let ctx = this.context
+
+        let space_size = this.space_size
+        let slit_size = this.slit_size
+
+        let size = this.size
+
+        let A_char = ' '
+
+        if (alphabet) {
+            A_char = init_char.charCodeAt(0)
+        } else {
+            A_char = init_char
+        }
+
+        ctx.font = Font
+
+        for (let i = 0; i < size; i++) {
+            let disp_char = ' '
+            if (alphabet) {
+                disp_char = String.fromCharCode(A_char + i)
+            } else {
+                disp_char = (A_char + i).toString()
+            }
+            let x = start_x + ((1 + i * x_inc) * slit_size) + (i * x_inc * space_size)
+            let y = start_y + ((1 + i * y_inc) * slit_size) + (i * y_inc * space_size)
+            ctx.fillText(disp_char, x, y, space_size)
+        }
+
+    }
+
+    drawPawnSpaces() {
+        let ctx = this.context
+        let size = this.size
+
+        let start_x = this.x_offset
+        let start_y = this.y_offset
+
+        let board_rep = this.board_rep
+
+        ctx.fillStyle = SpaceColor
+
+        for (let x = 0; x < size; x++) {
+            for (let y = 0; y < size; y++) {
+                board_rep[x][y].draw()
+            }
+        }
+
+
     }
 }
 
@@ -100,12 +212,39 @@ class PawnSpace {
      * @param {Pawn} pawn, the pawn on the space or null
      * @param {[Wall]} walls, the walls adjacent to the piece
      */
-    constructor(x, y) {
+    constructor(board, x, y) {
+        this.board = board
+
         this.x = x;
         this.y = y;
         this.pawn = null;
         this.walls = null;
+
+
+        //compute real x and y (shouldn't need to change)
+        let start_x = board.x_offset
+        let start_y = board.y_offset
+        let space_size = board.space_size
+        let slit_size = board.slit_size
+
+        this.real_x = start_x + ((1 + x) * slit_size) + (x * space_size);
+        this.real_y = start_y + ((1 + y) * slit_size) + (y * space_size);
     }
+
+    draw() {
+
+        let board = this.board
+        let ctx = board.context
+
+
+        let space_size = board.space_size
+
+
+        ctx.fillStyle = SpaceColor;
+        ctx.fillRect(this.real_x, this.real_y, space_size, space_size);
+
+    }
+
 }
 class Pawn {
     /**
@@ -166,53 +305,91 @@ class Pawn {
             this.setSpace(space)
             return true
         } else if (dist == 2) {
-            //TODO
+            //TODO check that pawn between third space in that cardinal direction
+            //AND (wall blocking the way between that space and the third ''') OR (pawn on the third ''') 
+        } else if (dist == 3) {
+            //TODO check that other pawn in front
         }
+    }
+
+    draw() {
+        let space = this.space
+        let x = space.real_x
+        let y = space.real_y
+
+        let board = this.space.board
+
+        let space_size = board.space_size
+        let h_size = space_size / 2
+
+        let display_x = x + h_size
+        let display_y = y + h_size
+
+        let ctx = board.context
+
+        let radius_size = (board.space_size * PawnRatio) / 2
+
+        ctx.beginPath()
+        ctx.fillStyle = this.color
+        ctx.arc(display_x, display_y, radius_size, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath()
+
+
+
     }
 }
 class Wall {
     //if it doesn't go down, it goes right
-    constructor(x, y, goes_down) {
-        this.x = x;
-        this.y = y;
-        this.goes_down = goes_down;
+    constructor(parent_space, placed_by, goes_down) {
+        this.space = parent_space
+        this.x = parent_space.x
+        this.y = parent_space.y
+        this.goes_down = goes_down
+        return this.setWall(x, y, goes_down, placed_by)
     }
     blocks(space1, space2) {
-        //if they're offset on the same axis as the wall blocks
-        if ((this.goes_down && absolute(space1.x - space2.x) > 0) || (!this.goes_down && absolute(space1.y - space2.y) > 0)) {
-            //if both spaces are adjacent to the wall true
-            let s1w = space1.walls
-            let s2w = space2.walls
-            for (let i1 = 0; i1 < s1w.length; i1++) {
-                for (let i2 = 0; i2 < s2w.length; i2++) {
-                    if (s1w[i1] === s2w[i2] && s1w[i1] === this)
-                        return true
+            //if they're offset on the same axis as the wall blocks
+            if ((this.goes_down && absolute(space1.x - space2.x) > 0) || (!this.goes_down && absolute(space1.y - space2.y) > 0)) {
+                //if both spaces are adjacent to the wall true
+                let s1w = space1.walls
+                let s2w = space2.walls
+                for (let i1 = 0; i1 < s1w.length; i1++) {
+                    for (let i2 = 0; i2 < s2w.length; i2++) {
+                        if (s1w[i1] === s2w[i2] && s1w[i1] === this)
+                            return true
+                    }
                 }
             }
-        }
-        return false
-    }
-    setWall(x, y, goes_down, placed_by) {
-        if (placed_by.wall_nb <= 0) {
-            console.log("pawn tried to place a wall they didn't have")
             return false
         }
+        //returns null if invalid, updates board and returns this if valid
+    setWall(x, y, goes_down, placed_by) {
+        let board = this.space.board
+
+        if (placed_by.wall_nb <= 0) {
+            console.log("pawn tried to place a wall they didn't have")
+            return null
+        }
         //check no wall starting on same space
-        let origin_space = Board[x][y];
+        let origin_space = board[x][y];
         let o_walls = origin_space.walls;
 
-        for (let i = 0; i < o_walls.length; i++) {
-            let wall = o_walls[i]
-            if (wall.x === x && wall.y === y)
-                return false
+
+        if (o_walls) {
+            for (let i = 0; i < o_walls.length; i++) {
+                let wall = o_walls[i]
+                if (wall.x === x && wall.y === y)
+                    return null
+            }
         }
 
         //check no wall in the same direction starting next space
         let next_space = null
         if (goes_down)
-            next_space = Board[x][y - 1]
+            next_space = board[x][y - 1]
         else
-            next_space = Board[x + 1][y]
+            next_space = board[x + 1][y]
 
         let n_walls = next_space.walls;
 
@@ -220,87 +397,51 @@ class Wall {
             let wall = n_walls[i]
             if (wall.x === x && wall.y === y)
                 if (wall.goes_down === goes_down)
-                    return false
+                    return null
         }
         //known OK the fact that wall only go down and right
         //TODO: Add the wall to the list of adjacent walls for all relevant spaces
         //TODO: Add to the wall list if we decide to use it(mostly for drawing)
+        return this
     }
+    draw() {
+        let space = this.space
+        let board = space.board
+        let space_size = board.space_size
+        let slit_size = board.slit_size
 
-}
+        let slit_length = space_size + slit_size + space_size
 
+        let orig_x = space.real_x
+        let orig_y = space.real_y
+        let target_x = 0
+        let target_y = 0
 
-/**
- * Draws everything
- * @param {html5-canvas-context} context 
- */
-function drawBoard(ctx) {
-
-    drawPawnSpaces(ctx, BoardOffsetX, BoardOffsetY)
-
-    let H_x = BoardOffsetX + intDiv(SpaceSize, 2) - SlitSize
-    let H_y = BoardOffsetY + intDiv(SpaceSize, 2) + ((1 + BoardSize) * SlitSize) + (BoardSize * SpaceSize)
-    let V_x = BoardOffsetX - SpaceSize
-    let V_y = BoardOffsetY + ((1 + BoardSize) * SlitSize) + (BoardSize * SpaceSize) - intDiv(SpaceSize, 2)
-    drawAxis(ctx, V_x, V_y, 1, 0, -1, false)
-    drawAxis(ctx, H_x, H_y, 'A', 1, 0, true)
-
-}
-
-function drawPawnSpaces(ctx, start_x, start_y) {
-
-    ctx.fillStyle = SpaceColor
-
-    display_x = 0
-    display_y = 0
-
-    for (let x = 0; x < BoardSize; x++) {
-        for (let y = 0; y < BoardSize; y++) {
-            drawPawnSpace(ctx, Board[x][y], start_x, start_y);
-        }
-    }
-
-
-}
-
-function drawPawnSpace(ctx, space, start_x, start_y) {
-    let x = space.x;
-    let y = space.y;
-
-    let display_x = start_x + ((1 + x) * SlitSize) + (x * SpaceSize);
-    let display_y = start_y + ((1 + y) * SlitSize) + (y * SpaceSize);
-
-    ctx.fillStyle = SpaceColor;
-    ctx.fillRect(display_x, display_y, SpaceSize, SpaceSize);
-
-}
-
-function drawAxis(ctx, start_x, start_y, init_char, x_inc, y_inc, alphabet) {
-
-    let A_char = ' '
-
-    if (alphabet) {
-        A_char = init_char.charCodeAt(0)
-    } else {
-        A_char = init_char
-    }
-
-    ctx.font = Font
-
-    for (let i = 0; i < BoardSize; i++) {
-        console.log(A_char)
-        let disp_char = ' '
-        if (alphabet) {
-            disp_char = String.fromCharCode(A_char + i)
+        if (this.goes_down) {
+            orig_x = orig_x + space_size
+            target_x = orig_x + slit_size
+            target_y = orig_y + slit_length
         } else {
-            disp_char = (A_char + i).toString()
+            orig_y = orig_y + space_size
+            target_x = orig_x + slit_length
+            target_y = orig_y + slit_size
         }
-        let x = start_x + ((1 + i * x_inc) * SlitSize) + (i * x_inc * SpaceSize)
-        let y = start_y + ((1 + i * y_inc) * SlitSize) + (i * y_inc * SpaceSize)
-        ctx.fillText(disp_char, x, y, SpaceSize)
+
+        let ctx = board.context
+        ctx.fillStyle = WallColor
+        ctx.fillRect(orig_x, orig_y, target_x, target_y)
     }
 
 }
+
+
+
+
+
+
+
+
+
 
 //may not be needed, draw them when drawing spaces
 function drawPawns() {
